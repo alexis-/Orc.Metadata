bd9c09cd0763f8e5b060447aa6797bb4d05c488e
Alexis <alexis.pujo@gmail.com>
2016-09-28 19:58:19 +0200

    * Merged feature/documentation * Merged feature/indexers

--- src/Orc.Metadata/Orc.Metadata.Shared/Models/Interfaces/IObjectWithMetadata.cs
+++ src/Orc.Metadata/Orc.Metadata.Shared/Models/Interfaces/IObjectWithMetadata.cs
@@ -1,18 +1,39 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IObjectWithMetadata.cs" company="WildGums">
-//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
+//   Copyright (c) 2008 - 2016 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-
 namespace Orc.Metadata
 {
+    /// <summary>
+    /// Holds a <see cref="IMetadataCollection"/> with its associated target object instance.
+    /// </summary>
     public interface IObjectWithMetadata
     {
+        #region Properties
+
+        /// <summary>Target object instance.</summary>
         object Instance { get; }
+
+        /// <summary><see cref="IMetadata"/> properties which can be applied to the given <see cref="Instance"/>.</summary>
         IMetadataCollection MetadataCollection { get; }
 
+        #endregion
+
+
+
+        #region Methods
+
+        /// <summary>Retrieves the value from the metadata with the given name if available.</summary>
+        /// <param name="key">The property name.</param>
+        /// <returns>Value if available or NULL.</returns>
         object GetMetadataValue(string key);
+
+        /// <summary>Sets the value of the metadata with the given name if available.</summary>
+        /// <param name="key">The property name.</param>
+        /// <param name="value">The value to be set.</param>
+        /// <returns>Whether the operation succeeded.</returns>
         bool SetMetadataValue(string key, object value);
 
         /// <summary>
@@ -23,5 +44,7 @@ namespace Orc.Metadata
         /// <param name="propertyKey">The property key.</param>
         /// <returns><see cref="IMetadata"/> value for associated model if available, null otherwise.</returns>
         object this[string propertyKey] { get; set; }
+
+        #endregion
     }
 }
--- src/Orc.Metadata/Orc.Metadata.Shared/Providers/Interfaces/IMetadataProvider.cs
+++ src/Orc.Metadata/Orc.Metadata.Shared/Providers/Interfaces/IMetadataProvider.cs
@@ -1,18 +1,37 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IMetadataProvider.cs" company="WildGums">
-//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
+//   Copyright (c) 2008 - 2016 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-
 
 namespace Orc.Metadata
 {
     using System.Threading.Tasks;
 
+    /// <summary>
+    ///     Factory-pattern interface which generates a <see cref="IObjectWithMetadata" />
+    ///     with the provided model instance.
+    /// </summary>
     public interface IMetadataProvider
     {
+        #region Methods
+
+        /// <summary>
+        ///     Synchronously generates a <see cref="IObjectWithMetadata" /> with the given object
+        ///     instance.
+        /// </summary>
+        /// <param name="obj">Object instance.</param>
+        /// <returns>Generated object.</returns>
         [ObsoleteEx(RemoveInVersion = "2.0.0", TreatAsErrorFromVersion = "1.1.0", ReplacementTypeOrMember = "GetMetadataAsync")]
         IObjectWithMetadata GetMetadata(object obj);
+
+        /// <summary>
+        ///     Asynchronously generates a <see cref="IObjectWithMetadata" /> with the given object
+        ///     instance.
+        /// </summary>
+        /// <param name="obj">Object instance.</param>
+        /// <returns>Waitable task.</returns>
         Task<IObjectWithMetadata> GetMetadataAsync(object obj);
+        #endregion
     }
 }
